7d7d07d259f5bbfa934305af1400a7e41257aa52
Learndev-pro <dehariyasrajan0149@gmail.com>
2022-02-02 00:22:02 +0000

    1.0

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ async function d_import (url)
 	{
 		let str = url.replace("https://learndev-pro.github.io/VigyMat",'') ;
 		history.pushState({},'',url) ;
-		let { module } = await import(`https://learndev-pro.github.io/VigyMat/js${str}.js`) ;
+		let module = await import(`https://raw.githubusercontent.com/Learndev-pro/VigyMat/main/js/Geometry/cube.js${str}.js`) ;
 		render( module.html );
 	}catch(error)
 	{
